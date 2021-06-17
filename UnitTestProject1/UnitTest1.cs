54b7705a847bd913d884d14b3bd14e550ff216f7
yahyabouhlal <yahya.bouhlal.b@gmail.com>
2021-06-17 12:00:44 +0000

    unittestCalculator

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Calculatorr;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange  
+            
+            double expectedResult = 16;
+
+
+            //Act  
+
+            double actualResult = Calculator.Add(8, 8);
+
+
+            //Assert  
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
